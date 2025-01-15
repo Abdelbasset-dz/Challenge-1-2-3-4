c9b25c963941d841e572807dbed3c58d83952dec
Abdelbasset-dz <sahara_alg@hotmail.com>
2025-01-15 21:01:35 +0100

    challenge-three script file

---
+++ challenge three.js
@@ -0,0 +1,15 @@
+let min = 0;
+let max = 3;
+
+let n = Math.floor(Math.random() * (max + 1));
+if (n == 0) {
+  console.log("your fate is “A certain victory”");
+}
+else if (n == 1) {
+  console.log('your fate is “Not so certain victory”');
+}
+else if (n == 2) {
+  console.log("your fate is “An uneasy victory”");
+}
+else
+console.log("Your fate is unclear, ô chosen undead")
